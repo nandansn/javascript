9282c2e5c5668eb63bc6cf30aeab3a3986affaae
Nandakumar R <geeknanda13@gmail.com>
2021-07-29 09:51:30 +0000

    updated

---
+++ lab/for.js
@@ -0,0 +1,23 @@
+let name = "nandakumar";
+
+let nameChars = Array.from(name);
+
+console.log(nameChars);
+
+for (let itm of nameChars) {
+  console.log(itm);
+}
+
+
+let person = {
+  name: 'nanda',
+  age:40,
+  location:'erode'
+
+}
+
+console.log(person);
+
+for (let key in person) {
+  console.log(person[key]);
+}
